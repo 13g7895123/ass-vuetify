2f182e521db30d15b50309a5496be9a584e5db69
Jarvis <13g7895123@gmail.com>
2023-07-13 00:10:55 +0800

    [edit] change icon

--- vue-app/src/components/Sidebar.vue
+++ vue-app/src/components/Sidebar.vue
@@ -23,7 +23,7 @@
 
     <v-list density="compact" nav>
         <v-list-item prepend-icon="mdi-home-city" title="Home" value="home"></v-list-item>
-        <v-list-item prepend-icon="mdi-account" title="My Account" value="account"></v-list-item>
+        <v-list-item prepend-icon="mdi-cash-plus" title="My Account" value="account"></v-list-item>
         <v-list-item prepend-icon="mdi-account-group-outline" title="Users" value="users"></v-list-item>
     </v-list>
     </v-navigation-drawer>
