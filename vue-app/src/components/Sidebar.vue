419cf0bff5bddef63c39f517526cff33b9d6f3d9
Jarvis <13g7895123@gmail.com>
2023-07-13 08:32:12 +0800

    test u2 template

--- vue-app/src/components/Sidebar.vue
+++ vue-app/src/components/Sidebar.vue
@@ -1,34 +1,62 @@
 <template>
     <v-navigation-drawer
-    v-model="drawer"
-    :rail="rail"
-    permanent
-    @click="rail = false"
-    >
-    <v-list-item
-        prepend-avatar="https://randomuser.me/api/portraits/men/85.jpg"
-        title="John Leider"
-        nav
-    >
-        <template v-slot:append>
-        <v-btn
-            variant="text"
-            icon="mdi-chevron-left"
-            @click.stop="rail = !rail"
-        ></v-btn>
-        </template>
-    </v-list-item>
+        v-model="drawer"
+        app
+        color="white"
+       
+        mini-variant
+        mini-variant-width="80"
+      >
+        <v-avatar
+          class="d-block text-center mx-auto mt-4"
+          color=""
+          size="40"
+        >
+        <v-icon color="#49D9A0" large>fab fa-atlassian</v-icon>
+        </v-avatar>
 
-    <v-divider></v-divider>
+        <v-divider class="mx-3 my-5"></v-divider>
+         
+       <v-list flat  class="mt-5" >
+            <v-list-item-group v-model="selectedItem"  >
+              
+                <v-list-item v-for="(item, i) in items" :key="i" active-class="border" class=""
+                :ripple="false"
+                >
+                    <v-list-item-icon >    
+                        <v-icon v-text="item.icon"></v-icon>
+                    </v-list-item-icon>
+                    </v-list-item> 
+            </v-list-item-group>
+        </v-list>
+        <div style="position: absolute; bottom: 20px;
+        margin-left: auto; margin-right: auto; left:0; right: 0; text-align: center;">
+                <v-btn icon :ripple="false">
+         <v-badge bordered overlap color="red" dot>
+        
+        <v-icon>far fa-bell</v-icon>
+     
+         </v-badge>
+          </v-btn><br>
+           <v-avatar size="30"  >
+          <v-img src="https://cdn.vuetifyjs.com/images/lists/2.jpg"></v-img>
+        </v-avatar>
 
-    <v-list density="compact" nav>
-        <v-list-item prepend-icon="mdi-home-city" title="Home" value="home"></v-list-item>
-        <v-list-item prepend-icon="mdi-cash-plus" title="My Account" value="account"></v-list-item>
-        <v-list-item prepend-icon="mdi-account-group-outline" title="Users" value="users"></v-list-item>
-    </v-list>
-    </v-navigation-drawer>
+        </div>
+      </v-navigation-drawer>
+
 </template>
 <script setup>
-const drawer = true
-const rail = true
+// const drawer = true
+// const rail = true
+
+const selectedItem = 0
+const drawer = null
+const item =  [
+    {icon: 'fas fa-plus'},
+    {icon: 'fas fa-th-large'},
+    {icon: 'fas fa-align-center'},
+    {icon: 'fab fa-gitter'},
+    {icon: 'fas fa-chart-line'},
+]
 </script>
