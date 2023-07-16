5a31f813dd63469dcc05f9c387d860d1ccfb9e11
Jarvis <13g7895123@gmail.com>
2023-07-16 18:02:52 +0800

    [edit] test debug

--- vue-app/src/main.js
+++ vue-app/src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue'
 import vuetify from './plugins/vuetify'
 import { loadFonts } from './plugins/webfontloader'
 import route from "./router"
+import 'vuetify/dist/vuetify.min.css';
 
 loadFonts()
 
