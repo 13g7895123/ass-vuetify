cbb08ab6d9875a9ce656858cdd5299654aca7690
Jarvis <13g7895123@gmail.com>
2023-07-16 23:27:57 +0800

    [edit] use v-card to revise render

--- vue-app/src/components/form/NewBonus.vue
+++ vue-app/src/components/form/NewBonus.vue
@@ -1,49 +1,52 @@
 <template>
-    <form @submit.prevent="submit">
-        <v-text-field
-        label="Case"
-        ></v-text-field>
+    <v-card>
+        <v-card-title>New Bonus</v-card-title>
+        <form @submit.prevent="submit">
+            <v-text-field
+            label="Case"
+            ></v-text-field>
 
-        <v-select
-        v-model="selectCate.value"
-        :items="categories.value"
-        label="Category"
-        ></v-select>
+            <v-select
+            v-model="selectCate.value"
+            :items="categories.value"
+            label="Category"
+            ></v-select>
 
-        <v-text-field
-        label="Phone Number"
-        ></v-text-field>
+            <v-text-field
+            label="Phone Number"
+            ></v-text-field>
 
-        <v-text-field        
-        label="E-mail"
-        ></v-text-field>
+            <v-text-field        
+            label="E-mail"
+            ></v-text-field>
 
-        <!-- <v-select
-        v-model="select.value.value"
-        :items="items"
-        :error-messages="select.errorMessage.value"
-        label="Select"
-        ></v-select> -->
+            <!-- <v-select
+            v-model="select.value.value"
+            :items="items"
+            :error-messages="select.errorMessage.value"
+            label="Select"
+            ></v-select> -->
 
-        <!-- <v-checkbox
-        v-model="checkbox.value.value"
-        :error-messages="checkbox.errorMessage.value"
-        value="1"
-        label="Option"
-        type="checkbox"
-        ></v-checkbox> -->
+            <!-- <v-checkbox
+            v-model="checkbox.value.value"
+            :error-messages="checkbox.errorMessage.value"
+            value="1"
+            label="Option"
+            type="checkbox"
+            ></v-checkbox> -->
 
-        <v-btn
-        class="me-4"
-        type="submit"
-        >
-        submit
-        </v-btn>
+            <v-btn
+            class="me-4"
+            type="submit"
+            >
+            submit
+            </v-btn>
 
-        <v-btn @click="">
-        clear
-        </v-btn>
-    </form>
+            <v-btn @click="">
+            clear
+            </v-btn>
+        </form>
+    </v-card>
 </template>
 <script setup>
 import { ref } from 'vue'
