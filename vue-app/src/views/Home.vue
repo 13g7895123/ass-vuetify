b81305588e8fb83d83535126c066a4bd08692627
Jarvis <13g7895123@gmail.com>
2023-07-16 09:55:44 +0800

    [edit] remove remark code

--- vue-app/src/views/Home.vue
+++ vue-app/src/views/Home.vue
@@ -1,8 +1,6 @@
 <template>
     <Sidebar />
     <router-view />
-    <!-- <label class="text-red">test text</label>
-    <h1>this is home page</h1> -->
 </template>
 <script setup>
 import Sidebar from '../components/Sidebar.vue';
