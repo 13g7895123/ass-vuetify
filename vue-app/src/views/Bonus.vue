4480333cc5347f2f6d8376e55bf474f2eca4b703
Jarvis <13g7895123@gmail.com>
2023-07-16 13:38:51 +0800

    [add] padding x

--- vue-app/src/views/Bonus.vue
+++ vue-app/src/views/Bonus.vue
@@ -7,7 +7,7 @@
             新增案件
         </v-btn>
     </v-row>
-    <v-row>
+    <v-row class="px-3">
         <v-data-table-server
             v-model:items-per-page="itemsPerPage"
             :headers="headers"
