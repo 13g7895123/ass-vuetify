2698e5772ae8097f20ac65cf7b575263eea6e6c3
Jarvis <13g7895123@gmail.com>
2023-07-16 22:40:22 +0800

    [edit] remove useless btn

--- vue-app/src/components/dialog/NewBonus.vue
+++ vue-app/src/components/dialog/NewBonus.vue
@@ -3,15 +3,6 @@
       v-model="props.showDialog"
       width="auto"
     >
-      <template v-slot:activator="{ props }">
-        <v-btn
-          color="primary"
-          v-bind="props"
-        >
-          Open Dialog
-        </v-btn>
-      </template>
-
       <v-card>
         <v-card-text>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
