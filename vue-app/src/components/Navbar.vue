a647b3d41603848a55eed23b75203e8feb925f20
Jarvis <13g7895123@gmail.com>
2023-07-16 12:07:15 +0800

    [add] navbar page

---
+++ vue-app/src/components/Navbar.vue
@@ -0,0 +1,7 @@
+<template>
+    <v-app-bar color="white" app dark flat >
+        <v-avatar tile color="#E5F1FF">
+            <v-img src="" />
+        </v-avatar>
+    </v-app-bar>
+</template>
--- vue-app/src/views/Home.vue
+++ vue-app/src/views/Home.vue
@@ -1,7 +1,9 @@
 <template>
+    <Navbar />
     <Sidebar />
     <router-view />
 </template>
 <script setup>
+import Navbar from '../components/Navbar.vue';
 import Sidebar from '../components/Sidebar.vue';
 </script>
