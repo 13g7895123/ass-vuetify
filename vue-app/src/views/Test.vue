b2d827eb85fcbb69736c779509e1f1c3cfbf74b1
Jarvis <13g7895123@gmail.com>
2023-07-16 18:19:45 +0800

    [edit] test debug

--- vue-app/src/views/Test.vue
+++ vue-app/src/views/Test.vue
@@ -42,51 +42,51 @@
 <script setup>
 import { VTextField } from 'vuetify/lib/components/index.mjs';
 // import { ref } from 'vue'
-// import { useField, useForm } from 'vee-validate'
+import { useField, useForm } from 'vee-validate'
 
-//   const { handleSubmit, handleReset } = useForm({
-//     validationSchema: {
-//       name (value) {
-//         if (value?.length >= 2) return true
+  const { handleSubmit, handleReset } = useForm({
+    validationSchema: {
+      name (value) {
+        if (value?.length >= 2) return true
 
-//         return 'Name needs to be at least 2 characters.'
-//       },
-//       phone (value) {
-//         if (value?.length > 9 && /[0-9-]+/.test(value)) return true
+        return 'Name needs to be at least 2 characters.'
+      },
+      phone (value) {
+        if (value?.length > 9 && /[0-9-]+/.test(value)) return true
 
-//         return 'Phone number needs to be at least 9 digits.'
-//       },
-//       email (value) {
-//         if (/^[a-z.-]+@[a-z.-]+\.[a-z]+$/i.test(value)) return true
+        return 'Phone number needs to be at least 9 digits.'
+      },
+      email (value) {
+        if (/^[a-z.-]+@[a-z.-]+\.[a-z]+$/i.test(value)) return true
 
-//         return 'Must be a valid e-mail.'
-//       },
-//       select (value) {
-//         if (value) return true
+        return 'Must be a valid e-mail.'
+      },
+      select (value) {
+        if (value) return true
 
-//         return 'Select an item.'
-//       },
-//       checkbox (value) {
-//         if (value === '1') return true
+        return 'Select an item.'
+      },
+      checkbox (value) {
+        if (value === '1') return true
 
-//         return 'Must be checked.'
-//       },
-//     },
-//   })
-//   const name = useField('name')
-//   const phone = useField('phone')
-//   const email = useField('email')
-//   const select = useField('select')
-//   const checkbox = useField('checkbox')
+        return 'Must be checked.'
+      },
+    },
+  })
+  const name = useField('name')
+  const phone = useField('phone')
+  const email = useField('email')
+  const select = useField('select')
+  const checkbox = useField('checkbox')
 
-//   const items = ref([
-//     'Item 1',
-//     'Item 2',
-//     'Item 3',
-//     'Item 4',
-//   ])
+  const items = ref([
+    'Item 1',
+    'Item 2',
+    'Item 3',
+    'Item 4',
+  ])
 
-//   const submit = handleSubmit(values => {
-//     alert(JSON.stringify(values, null, 2))
-//   })
+  const submit = handleSubmit(values => {
+    alert(JSON.stringify(values, null, 2))
+  })
 </script>
