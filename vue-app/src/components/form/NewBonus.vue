7566c7064883e57982a763d81ea66bdd96e5101c
Jarvis <13g7895123@gmail.com>
2023-07-16 23:18:46 +0800

    [edit] new column

---
+++ vue-app/src/components/form/NewBonus.vue
@@ -0,0 +1,54 @@
+<template>
+    <form @submit.prevent="submit">
+        <v-text-field
+        label="Case"
+        ></v-text-field>
+
+        <v-select
+        v-model="selectCate.value"
+        :items="categories.value"
+        label="Category"
+        ></v-select>
+
+        <v-text-field
+        label="Phone Number"
+        ></v-text-field>
+
+        <v-text-field        
+        label="E-mail"
+        ></v-text-field>
+
+        <!-- <v-select
+        v-model="select.value.value"
+        :items="items"
+        :error-messages="select.errorMessage.value"
+        label="Select"
+        ></v-select> -->
+
+        <!-- <v-checkbox
+        v-model="checkbox.value.value"
+        :error-messages="checkbox.errorMessage.value"
+        value="1"
+        label="Option"
+        type="checkbox"
+        ></v-checkbox> -->
+
+        <v-btn
+        class="me-4"
+        type="submit"
+        >
+        submit
+        </v-btn>
+
+        <v-btn @click="">
+        clear
+        </v-btn>
+    </form>
+</template>
+<script setup>
+
+const categories = ref(['web', 'python'])
+const selectCate = ref('')
+
+const submit = () => {}
+</script>
