662ea2a2bc34fe785c1fad1af54a06c92cf8aa0a
Jarvis <13g7895123@gmail.com>
2023-07-16 22:35:38 +0800

    [add] import ref

--- vue-app/src/components/dialog/NewBonus.vue
+++ vue-app/src/components/dialog/NewBonus.vue
@@ -17,7 +17,7 @@
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
         </v-card-text>
         <v-card-actions>
-          <v-btn color="primary" block @click="dialog = false">Close Dialog</v-btn>
+          <v-btn color="primary" block @click="emits('closeDialog')">Close Dialog</v-btn>
         </v-card-actions>
       </v-card>
     </v-dialog>
