2be6dafb9797738441d03fb1ae4c0abf02eddfae
Jarvis <13g7895123@gmail.com>
2023-07-16 23:03:03 +0800

    [edit] btn render

--- vue-app/src/views/Bonus.vue
+++ vue-app/src/views/Bonus.vue
@@ -1,7 +1,7 @@
 <template>
     <v-row class="px-3 mt-3">
         <v-btn 
-            color="blue-accent-2"
+            color="blue-accent-3"
             prepend-icon="mdi-plus-circle-outline"
             variant="tonal"
             @click="handleAddBtn"
